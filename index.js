990d4565daa7dea805ed2f6598a0bdb15d2f0646
T-vK <t-vk@users.noreply.github.com>
2018-08-31 20:00:00 +0000

    Added zip support; can generate anki database now

--- index.js
+++ index.js
@@ -5,23 +5,102 @@ const packageInfo = require('./package.json')
 const program = require('commander')
 const fs = require('fs-extra')
 const AnkiDeckGenerator = require('./AnkiDeckGenerator')
-const ankiDeckGen = new AnkiDeckGenerator()
+const JSZip = require("jszip")
 
 program
-  .version(packageInfo.version)
-  .option('-c, --input-file-chinese <file-path>', 'File containing a json-array of Chinese characters, words and/or sentences')
-  .option('-o, --output-folder <folder-path>', 'Folder in which the deck files will be written')
-  .parse(process.argv)
+    .command('auto-generate <apkg-output-file>')
+    .option('-c, --input-file-chinese [file-path]', 'File containing a json-array of Chinese characters, words and/or sentences')
+    .option('-n, --deck-name <string>', 'Name of the deck to be created')
+    .option('-d, --deck-description <string>', 'Name of the deck to be created')
+    .option('-t, --temp-folder [folder-path]', 'Folder to be used/created for temporary files')
+    .action((apkgFile, cmd) => {
+        cmd.tempFolder = cmd.tempFolder || './anki-deck-generator-temp'
+        cmd.deckName = cmd.deckName || "NewDeck"
+        cmd.deckDescription = cmd.deckDescription || "A new deck"
+        let apkg
+        fs.emptyDir(cmd.tempFolder).then(() => {
+            apkg = new AnkiDeckGenerator(`${cmd.tempFolder}/${cmd.deckName}.anki2`)
+        }).then(() => {
+            return apkg.init()
+        }).then(() => {
+            return apkg.addDeck({
+                name: cmd.deckName,
+                desc: cmd.deckDescription
+            })
+        }).then(() => {
+            return fs.readdir(cmd.tempFolder)
+        }).then(files => {
+            const apkgArchive = new JSZip()
+            const filepathArr = files.map(filename=>`${cmd.tempFolder}/${filename}`)
+            filepathArr.forEach(filepath => {
+                console.log(filepath)
+                apkgArchive.file(filepath, CONTENT) // <-- TODO! https://stuk.github.io/jszip/documentation/api_jszip/generate_async.html
+            })
+            return apkgArchive.generateAsync({type:"uint8array"})
+        }).then(content => {
+            console.log(content)
+            return fs.writeFile(apkgFile, content)
+        }).then(() => {
+            console.log("Done!")
+        }).catch(console.error)
+    })
 
-if (!program.outputFolder || !program.inputFileChinese) {
-    throw new Error("Error, missing required parameter(s)!")
+    program
+        .command('update <file-path>')
+        .option('-c, --input-file-chinese <file-path>', 'File containing a json-array of Chinese characters, words and/or sentences')
+        .option('-t, --temp-folder <folder-path>', 'Folder to be used/created for temporary files')
+        .action((apkgFile, cmd) => {
+            cmd.tempFolder = cmd.tempFolder || './anki-deck-generator-temp'
+            fs.emptyDir(cmd.tempFolder).then(() => {
+                return fs.readFile(apkgFile)
+            }).then(zipData => {
+                return JSZip.loadAsync(zipData)
+            }).then(zip => {
+                for (const filename of Object.keys(zip.files)) {
+                    if (filename.includes('.anki2')) {
+                        zip.files[filename].async('uint8array').then(fileData => {
+                            return fs.writeFile(`${cmd.tempFolder}/${filename}`, fileData)
+                        })
+                    }
+                }
+            }).then(() => {
+                console.log("Done!")
+            }).catch(console.error)
+        })
+
+program.parse(process.argv)
+/*
+if (!program.apkgFile || !program.inputFileChinese) {
+    throw new Error("Errorreturn fs.readFile(apkgFile)
+        }).then(zipData => {
+            return JSZip.loadAsync(zipData)
+        }).then(zip => {
+            for (const filename of Object.keys(zip.files)) {
+                if (filename.includes('.anki2')) {
+                    zip.files[filename].async('uint8array').then(fileData => {
+                        return fs.writeFile(`${cmd.tempFolder}/${filename}`, fileData)
+                    })
+                }
+            }, missing required parameter(s)!")
 }
+program.tempFolder = program.tempFolder || './temp'
 
-let tsvOutput = ''
-fs.ensureDir(program.outputFolder).then(() => {
+const apkg = new AnkiDeckGenerator('./db.anki2')
+
+fs.ensureDir(program.tempFolder).then(() => {
     return fs.readFile(program.inputFileChinese,'utf8')
 }).then(fileContents => {
-    const chineseInputArr = fileContents.split(/\r?\n/)
+    return apkg.init()
+}).then(() => {
+    return apkg.addDeck({
+        name: "NewDeck",
+        desc: "Test deck"
+    })
+}).then(() => {
+    console.log("Done!")
+}).catch(console.error)
+*/
+    /*const chineseInputArr = fileContents.split(/\r?\n/)
     return ankiDeckGen.mmah.getCharData(chineseInputArr).then(dataObj => {
         for (let key in dataObj) {
             let item = dataObj[key]
@@ -45,8 +124,12 @@ fs.ensureDir(program.outputFolder).then(() => {
         }
         return tsvOutput
     })
-}).then(tsvOutput => {
+})*/
+
+/*
+.then(tsvOutput => {
     return fs.writeFile(`${program.outputFolder}/data.txt`, tsvOutput)
 }).then(() => {
     console.log('Done!')
 }).catch(console.error)
+*/
